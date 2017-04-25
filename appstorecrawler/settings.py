4cd088ee631b498a593ec8b26bb043eb76ed6993
Le Zhang <zhrobinson@gmail.com>
2017-04-25 10:21:40 -0500

    update proxy settings

--- appstorecrawler/settings.py
+++ appstorecrawler/settings.py
@@ -36,7 +36,34 @@ MONGODB_COLLECTION = "metadata"
 
 #DOWNLOAD_DELAY = 0.5
 
+#DOWNLOADER_MIDDLEWARES = {
+#    'scrapyproduct.middlewares.ProxyMiddleware': 1,
+#    'scrapyproduct.middlewares.DelayAfterConnectionRefusedMiddleware': 510,
+#}
+
+# Retry many times since proxies often fail
+RETRY_TIMES = 5
+# Retry on most error codes since proxies fail for different reasons
+RETRY_HTTP_CODES = [500, 503, 504, 400, 403, 404, 408]
+
 DOWNLOADER_MIDDLEWARES = {
-    'scrapyproduct.middlewares.ProxyMiddleware': 1,
-    'scrapyproduct.middlewares.DelayAfterConnectionRefusedMiddleware': 510,
+    'scrapy.downloadermiddlewares.retry.RetryMiddleware': 90,
+    'scrapy_proxies.RandomProxy': 100,
+    'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 110,
 }
+
+# Proxy list containing entries like
+# http://host1:port
+# http://username:password@host2:port
+# http://host3:port
+# ...
+PROXY_LIST = 'list.txt'
+
+# Proxy mode
+# 0 = Every requests have different proxy
+# 1 = Take only one proxy from the list and assign it to every requests
+# 2 = Put a custom proxy to use in the settings
+PROXY_MODE = 0
+
+# If proxy mode is 2 uncomment this sentence :
+#CUSTOM_PROXY = "http://host1:port"
